44933fe79c22b461f2f37c88c9d3aabe07612cde
heartedmachine <r.melissa@icloud.com>
2015-02-03 13:20:39 -0500

    feat(all): setting structure for header & footer

--- js/text-carousel.js
+++ js/text-carousel.js
@@ -1,19 +1,9 @@
-$(document).ready(function() {
-
-  $("#owl-demo").owlCarousel({
-
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true
-
-      // "singleItem:true" is a shortcut for:
-      // items : 1,
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
-
-  });
-
-});
+ $('.single-items').slick({
+     dots: true,
+     infinite: true,
+     speed: 400,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     autoplay: true,
+     arrows: true
+ });
