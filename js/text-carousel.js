680c45507ba74b94666a3e738f41ab406e88517a
heartedmachine <r.melissa@icloud.com>
2015-02-03 20:45:32 -0500

    feat(all): transferred files from local to live

--- js/text-carousel.js
+++ js/text-carousel.js
@@ -1,4 +1,24 @@
- $('.single-items').slick({
+ $('.index-carousel').slick({
+     dots: true,
+     infinite: true,
+     speed: 400,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     autoplay: true,
+     arrows: true
+ });
+
+  $('.feature-quotes').slick({
+     dots: true,
+     infinite: true,
+     speed: 400,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     autoplay: true,
+     arrows: true
+ });
+
+ $('.blog-carousel').slick({
      dots: true,
      infinite: true,
      speed: 400,
