a7772ad9409c21d8c45d725d3e4a88aa470c6cb0
heartedmachine <r.melissa@icloud.com>
2015-02-08 12:16:05 -0500

    fix(css): make menu button change to close

--- js/push-menu.js
+++ js/push-menu.js
@@ -12,9 +12,13 @@ function toggleNav() {
   if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
     // Display Nav when closed
     $('.site-wrapper').attr('data-state', 'slide-open');
+    $('#menu-icon').addClass('icon-i-close');
+    $('#menu-icon').removeClass('icon-i-menu');
   } else {
     // Hide Nav when open
     $('.site-wrapper').attr('data-state', 'slide-closed');
+    $('#menu-icon').removeClass('icon-i-close');
+    $('#menu-icon').addClass('icon-i-menu');
   }
 }
 
