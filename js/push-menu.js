e9c9e0255ae7b42aa820ad97288f3161cefa7859
heartedmachine <r.melissa@icloud.com>
2015-02-17 13:36:18 -0500

    feat(file): add shrinking header

--- js/push-menu.js
+++ js/push-menu.js
@@ -1,3 +1,32 @@
+$(function(){
+    $('#header_nav').data('size','big');
+});
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 0)
+    {
+        if($('#header_nav').data('size') == 'big')
+        {
+            $('#header_nav').data('size','small');
+            $('#header_nav').stop().animate({
+                height:'80px'
+            },600);
+        }
+    }
+    else
+    {
+        if($('#header_nav').data('size') == 'small')
+        {
+            $('#header_nav').data('size','big');
+            $('#header_nav').stop().animate({
+                height:'120px'
+            },600);
+        }
+    }
+});
+
+
+
 $(function() {
   var $menu = $('nav#menu'),
     $html = $('html, body');
